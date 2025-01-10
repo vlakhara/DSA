cdba425849d4d96eab10fe3947082c2a04565f2a
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2025-01-10 22:12:16 +0530

    Time: 13 ms (81.40%) | Memory: 55.4 MB (15.12%) - LeetSync

---
+++ 952-word-subsets/word-subsets.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] maxFreq = new int[26];
+        List<String> result = new ArrayList<>();
+        
+        for (String word : words2) {
+            int[] freq = countFrequency(word);
+            for (int i = 0; i < 26; i++) {
+                maxFreq[i] = Math.max(maxFreq[i], freq[i]);
+            }
+        }
+        
+        for (String word : words1) {
+            int[] freq = countFrequency(word);
+            boolean isUniversal = true;
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] < maxFreq[i]) {
+                    isUniversal = false;
+                    break;
+                }
+            }
+            if (isUniversal) {
+                result.add(word);
+            }
+        }
+        
+        return result;
+
+    }
+
+      
+    private int[] countFrequency(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        return freq;
+    }
+}
+
+// abc aabc
