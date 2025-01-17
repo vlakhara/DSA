9bdcc42b6649dbf28167a4771f376ec58fd488d7
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2025-01-17 09:45:21 +0530

    Time: 2 ms (100.00%) | Memory: 62.7 MB (26.47%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
         int res = 0;
+
         for(int i : derived) {
             res ^= i;
         }
+
         return res == 0;
     }
 }
