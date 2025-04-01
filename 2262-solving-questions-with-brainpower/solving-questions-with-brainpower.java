6af729ee5f7352c33a9d6e85315825e37e8d845a
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2025-04-01 23:56:39 +0530

    Time: 5 ms (96.96%) | Memory: 108.2 MB (65.18%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -2,7 +2,20 @@ class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
         long[] dp = new long[n];
-        return backtrack(0, n, dp, questions);
+
+        for(int i = n - 1; i >= 0; i--) {
+            long point = questions[i][0];
+            long brainPower = questions[i][1];
+
+            int next = (int) i + 1 + (int) brainPower;
+
+            long choose = point + (next < n ? dp[next] : 0); 
+            long skip = (i + 1) < n ? dp[i + 1] : 0;
+
+            dp[i] = Math.max(choose, skip);
+        }
+
+        return dp[0];
     }
 
     public static long backtrack(int i, int n, long[] dp, int[][] questions) {
