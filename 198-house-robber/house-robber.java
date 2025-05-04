b422de66ce84c32165e5a66e6f28041158e06498
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2025-05-04 15:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (60.63%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,15 +3,25 @@ class Solution {
 
         int n = nums.length;
 
-        int[] dp = new int[n];
-
-        Arrays.fill(dp, -1);
 
         if(nums.length <= 1) {
             return nums[0];
         }
+
+        int[] dp = new int[n];
+
+        // Arrays.fill(dp, -1);
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1], nums[0]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[n - 1];
         
-        return Math.max(nums[0] + find(0, 2, nums, n, dp), nums[1] + find(0, 3, nums, n, dp));
+        // return Math.max(nums[0] + find(0, 2, nums, n, dp), nums[1] + find(0, 3, nums, n, dp));
     }
 
     public static int find(int money, int i, int[] arr, int n, int[] dp) {
