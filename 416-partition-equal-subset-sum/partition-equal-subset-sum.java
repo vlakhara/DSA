81c8ac0e4a9e834504a8f4884257f6f5934a21e5
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2025-05-24 13:04:25 +0530

    Time: 10 ms (98.32%) | Memory: 56.9 MB (27.30%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -16,24 +16,49 @@ class Solution {
 
         Boolean[][] t = new Boolean[n + 1][sum + 1];
 
-        for(int i = 0; i <= sum; i++) {
-            t[0][i] = false;
+        // for(int i = 0; i <= sum; i++) {
+        //     t[0][i] = false;
+        // }
+
+        // for(int i = 0; i <= n; i++) {
+        //     t[i][0] = true;
+        // }
+
+        // for(int i = 1; i <= n; i++) {
+        //     for(int j = 1; j <= sum; j++) {
+        //         if(nums[i - 1] <= j) {
+        //             t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+        //         } else {
+        //             t[i][j] = t[i - 1][j];
+        //         }
+        //     }
+        // }
+
+        // return t[n][sum];
+
+        return findSubset(nums, sum, n - 1, t);
+    }
+
+    boolean findSubset(int[] arr, int sum, int n, Boolean[][] t) {
+        if(sum == 0) {
+            return true;
         }
 
-        for(int i = 0; i <= n; i++) {
-            t[i][0] = true;
+        if(n == 0) {
+            return arr[n] == sum;
         }
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= sum; j++) {
-                if(nums[i - 1] <= j) {
-                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
-                } else {
-                    t[i][j] = t[i - 1][j];
-                }
-            }
+        if(t[n][sum] != null) {
+            return t[n][sum];
+        }
+
+        if(arr[n] <= sum) {
+            t[n][sum] = findSubset(arr, sum - arr[n], n - 1, t) || findSubset(arr, sum, n - 1, t);
+        } else {
+            t[n][sum] = findSubset(arr, sum, n - 1, t);
         }
 
         return t[n][sum];
+
     }
 }
