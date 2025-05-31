2b808b87947b9653067b16ec183b18a5e16b2d60
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2025-05-31 15:40:55 +0530

    Time: 17 ms (48.40%) | Memory: 44.8 MB (42.11%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -4,14 +4,34 @@ class Solution {
 
         int[][] dp = new int[n + 1][amount + 1];
 
+        for (int j = 1; j <= amount; j++) {
+            dp[0][j] = Integer.MAX_VALUE - 1;
+        }
+
         for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[i][j] = -1;
+            dp[i][0] = 0;
+        }
+        
+        // for (int i = 0; i <= n; i++) {
+        //     for (int j = 0; j <= amount; j++) {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
 
-        int res = exchange(coins, amount, n, dp);
-        return res == Integer.MAX_VALUE - 1 ? -1 : res;
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
+
+        // int res = exchange(coins, amount, n, dp);
+        // return res == Integer.MAX_VALUE - 1 ? -1 : res;
     }
 
     int exchange(int[] coins, int amount, int n, int[][] dp) {
