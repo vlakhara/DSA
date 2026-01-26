a9dd08c7e9bf09a5be22a8dd6a88c0f06ebcad2a
Vipul Lakhara <83634281+vlakhara@users.noreply.github.com>
2026-01-26 16:35:59 +0530

    Time: 21 ms (72.27%) | Memory: 64.1 MB (32.26%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -18,49 +18,49 @@ class Solution {
         return answers;
     }
 
-    private static void sort(int[] arr, int start, int end) {
-        if(start == end) {
-            return;
-        }
+    // private static void sort(int[] arr, int start, int end) {
+    //     if(start == end) {
+    //         return;
+    //     }
 
-        int mid = start + ((end - start) / 2);
+    //     int mid = start + ((end - start) / 2);
 
-        sort(arr, start, mid);
-        sort(arr, mid + 1, end);
-        merge(arr, start, mid, end);
-    }
+    //     sort(arr, start, mid);
+    //     sort(arr, mid + 1, end);
+    //     merge(arr, start, mid, end);
+    // }
 
-    private static void merge(int[] arr, int start, int mid, int end) {
-        int[] temp = new int[end - start + 1];
-        int left = start;
-        int right = mid + 1;
-        int index = 0;
+    // private static void merge(int[] arr, int start, int mid, int end) {
+    //     int[] temp = new int[end - start + 1];
+    //     int left = start;
+    //     int right = mid + 1;
+    //     int index = 0;
 
-        while(left <= mid && right <= end) {
-            if(arr[left] < arr[right]) {
-                temp[index] = arr[left];
-                left++;
-            } else {
-                temp[index] = arr[right];
-                right++;
-            }
-            index++;
-        }
+    //     while(left <= mid && right <= end) {
+    //         if(arr[left] < arr[right]) {
+    //             temp[index] = arr[left];
+    //             left++;
+    //         } else {
+    //             temp[index] = arr[right];
+    //             right++;
+    //         }
+    //         index++;
+    //     }
 
-        while(left <= mid) {
-            temp[index] = arr[left];
-            left++;
-            index++;
-        }
+    //     while(left <= mid) {
+    //         temp[index] = arr[left];
+    //         left++;
+    //         index++;
+    //     }
 
-        while(right <= end) {
-            temp[index] = arr[right];
-            right++;
-            index++;
-        }
+    //     while(right <= end) {
+    //         temp[index] = arr[right];
+    //         right++;
+    //         index++;
+    //     }
 
-        for(int i = start; i <= end ; i++) {
-            arr[i] = temp[i - start];
-        }
-    }
+    //     for(int i = start; i <= end ; i++) {
+    //         arr[i] = temp[i - start];
+    //     }
+    // }
 }
